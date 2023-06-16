d9077f8d3342704263e9c6661228754c38c8a4ad
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-16 17:39:42 +0300

    Hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int colly;
+
+	for (colly = 0 ; colly < 16 ; colly++)
+	{
+		if (colly < 10)
+		{
+			putchar(colly + '0');
+		} else
+		{
+			putchar(colly - 10 + 'a');
+		}
+		{
+			putchar('\n');
+		}
+	}
+	return (0);
+}
