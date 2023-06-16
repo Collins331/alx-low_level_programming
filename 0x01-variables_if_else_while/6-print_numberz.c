48aa66eb7ec7fe22e0bedc0f8821c6e3fd5b633b
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-16 17:12:04 +0300

    program that prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int collins;
+
+	for (collins = 0 ; collins < 10 ; collins++)
+	{
+		putchar(collins + '0');
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
