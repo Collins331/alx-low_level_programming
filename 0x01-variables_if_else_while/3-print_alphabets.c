149deab2e7ef90144c04a80196f4da638ad2c24b
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-16 16:18:41 +0300

    created alphABET file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char Collins;
+
+	for (Collins = 'a' ; Collins <= 'z' ; Collins++)
+	{
+		putchar(Collins);
+	}
+	for (Collins = 'A' ; Collins <= 'A' ; Collins++)
+	{
+		putchar(Collins);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
