9cc63e162e18f3d0b8c358e108b766d9aab41531
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-16 17:00:04 +0300

    numbers loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int I;
+
+	for (I = 0 ; I < 10 ; I++)
+
+	{
+		printf("%d", I);
+	}
+	{
+		printf("\n");
+	}
+	return (0);
+}
