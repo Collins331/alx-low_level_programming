f0ef887697329aded82eb6c64c7b9891dae5d372
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-20 21:53:39 +0300

    function that prints the alphabet,

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - entry point
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(char l);
+void printf_alphabet(void);
