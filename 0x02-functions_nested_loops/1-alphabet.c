cbf2f3ee9f7c5fdec2e984d24f9964bc0e6a294f
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-21 00:42:45 +0300

    function that checks for lowercase character

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *print_alphabet - entry point
 *
@@ -11,7 +10,7 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+		_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_islower - entry point
+*
+*@c: char type letter
+*
+*Return: always 1 (success)
+*/
+int _islower(int c)
+{
+	if (c <= 97 && c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
