916a39b8ca2a73f5cb0e14eed3b2d7c57e63e25d
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-21 21:31:09 +0300

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - entry point
+*
+*Return: always 0 (success)
+*/
+void positive_or_negative(int i)
+{
+	i = 98;
+	positive_or_negative(i);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+# DEBUGGING IN C PROGRAMMING
+
+* How to debug a code 
+* 
+* Various ways to debugg code
