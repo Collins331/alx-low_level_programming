43c63b1468ce3057777fd02e708f3fc6769802c1
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-21 21:56:30 +0300

    multiple lines

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-* main - entry point
-*
-*Return: always 0 (success)
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
