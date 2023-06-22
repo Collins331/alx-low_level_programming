d52d52f4ba32e4bac5e54a063b6a75c14afe0b06
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-06-22 18:55:45 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,25 @@ int main(void)
 
 	for (c = 1; c <= 100; c++)
 	{
-		if (c % 15 == 0)
+		if (c % 3 == 0 && c % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (c % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (c % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", c);
+			printf("%d", c);
+		}
+		if (c != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
