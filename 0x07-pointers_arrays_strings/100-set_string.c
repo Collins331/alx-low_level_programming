5cf883e5d16abd2b0f239e97f191f1a040c3cb8e
COLLINS ODUOR OCHIENG <125954874+Collins331@users.noreply.github.com>
2023-07-03 23:55:46 +0300

    Update 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+
+/**
+ *set_string - The Function that prints a value.
+ *@s: char type param
+ *@to: char type param
+ *Return: 0 if (successful).
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
