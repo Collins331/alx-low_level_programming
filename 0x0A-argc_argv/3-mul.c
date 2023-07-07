3c3efd0a540ac2950d4d848aa26e12edd7e554bd
COLLINS ODUOR OCHIENG <125954874+Collins331@users.noreply.github.com>
2023-07-07 21:36:55 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,10 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
-		argc++;
 	}
 	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
