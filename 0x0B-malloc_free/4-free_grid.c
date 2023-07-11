a274a7d34a221ef86861e60af3afdf85c5210d10
COLLINS ODUOR OCHIENG <125954874+Collins331@users.noreply.github.com>
2023-07-12 01:03:41 +0300

    Create 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *free_grid - frees 2 dimensional grid
+  *@grid: the grid previously created
+  *@height: the height of the grid
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
