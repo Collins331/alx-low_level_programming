e3dd9bf4c7cae815b065b41b1070a26ed48508d4
COLLINS ODUOR OCHIENG <125954874+Collins331@users.noreply.github.com>
2023-07-12 23:41:42 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,40 +1,45 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
-  *argstostr  - concatenates all the arguments of the program
-  *@ac: the argument count
-  *@av: the argument
-  *Return: always characters (success)
-  */
+ * argstostr -  a function that concatenates all the arguments
+ * of your program.
+ * @ac: argument count
+ * @av: double pointer to array of strings passed to main
+ * Return: Null if fail, else return pointer to new string
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int totalLength = 0;
-	int i;
-	char *result;
-	int current_index, argLength;
+	char *a, *retp;
+	int i, j, total;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
+
+	for (i = 0, total = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
 	}
+	total++;
+
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+
+	retp = a;
 	for (i = 0; i < ac; i++)
 	{
-		totalLength += strlen(av[i]) + 1;
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*a = av[i][j];
+			a++;
+		}
+		*a = '\n';
+		a++;
 	}
-	result = (char *)malloc((totalLength + 1) * sizeof(char));
 
-	if (result == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < ac; i++)
-	{
-		argLength = strlen(av[i]);
-		strncpy(result + current_index, av[i], argLength);
-		current_index += argLength;
-
-		result[current_index++] = '\n';
-	}
-	result[current_index] = '\0';
-
-	return (result);
+	return (retp);
 }
