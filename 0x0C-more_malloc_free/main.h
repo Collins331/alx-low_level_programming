7f6ca89e94c5d1f3ca83c151a15a8e779118299d
COLLINS ODUOR OCHIENG <125954874+Collins331@users.noreply.github.com>
2023-07-13 23:31:53 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,14 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <stddef.h>
-
 #include <string.h>
 #include <limits.h>
 
+void *malloc_checked(unsigned int b);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -18,7 +16,6 @@ void *malloc_checked(unsigned int b);
 int _putchar(char c);
 void errors(void);
 int _strlen(char *s);
-void *malloc_checked(unsigned int b);
 int is_digit(char *s);
 
 #endif
