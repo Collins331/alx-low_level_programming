ec7ba8540c8a31663f9406a71aab2b7c28d4d4b9
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-07-13 22:51:19 +0300

    function that creates an array of integers

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+
+#endif
