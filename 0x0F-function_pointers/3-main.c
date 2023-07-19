7dc0459d5c90c105e56ad7955a5a5ab9f8cb7bc0
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-07-19 23:44:05 +0300

    craeted 3-main.c file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+/**
+  *main - test the calculator
+  *@argc: the argument count
+  *@argv: the argument given
+  *Return: always result
+  */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = op_func(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
