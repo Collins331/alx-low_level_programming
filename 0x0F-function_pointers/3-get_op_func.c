80a7296087f4f1e6feba3b52f846a19e01998ee4
COLLINS ODUOR OCHIENG <125954874+Collins331@users.noreply.github.com>
2023-07-19 23:54:35 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,12 +19,10 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
-
 		}
-
 		i++;
 	}
 	return (NULL);
