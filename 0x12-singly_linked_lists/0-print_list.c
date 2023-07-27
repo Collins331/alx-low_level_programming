2e2da732b22124c1898f2edb14208b8ea874646d
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-07-27 21:09:17 +0300

    function that prints all the elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s",h->len, h->str);
+			printf("[%d] %s", h->len, h->str);
 		}
 		count++;
 
@@ -27,7 +27,7 @@ size_t print_list(const list_t *h)
 			printf("\n");
 		}
 	}
-	printf ("\n");
+	printf("\n");
 
 	return (count);
 }
