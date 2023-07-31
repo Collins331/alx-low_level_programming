87009649e670d0e1c983f1a33648b10fc822a95a
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-01 00:31:06 +0300

    frees the listint_t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,8 +6,14 @@
   */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
 	listint_t *next;
+	listint_t *current;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+	current = *head;
 
 	while (current != NULL)
 	{
