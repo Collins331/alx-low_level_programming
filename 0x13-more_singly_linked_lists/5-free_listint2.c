fa855e7faa5aef3c245a74c50c071ec849102934
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-01 00:35:42 +0300

    frees the listint_t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,14 +2,14 @@
 /**
   *free_listint2 - frees the listint_t
   *@head: a pointer to a pointer of linked list
-  *Return: always void
+  *Return: nothing
   */
 void free_listint2(listint_t **head)
 {
 	listint_t *next;
 	listint_t *current;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 	{
 		return;
 	}
