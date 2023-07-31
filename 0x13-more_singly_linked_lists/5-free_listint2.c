5481e65bddc7a8218cd44489e6fee0e5b0f0fd33
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-01 00:44:03 +0300

    frees the listint_t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,7 @@
 #include"lists.h"
 /**
   *free_listint2 - frees the listint_t
-  *@head: a pointer to a pointer of linked list
-  *Return: nothing
+  *@head: a pointer to a of linked list
   */
 void free_listint2(listint_t **head)
 {
