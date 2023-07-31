b2ebaeeef7e973dba55a0fcb7113b91c19f9a24f
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-07-31 22:01:24 +0300

    adds a new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,6 +8,10 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node ==	NULL)
+	{
+		return (NULL);
+	}
 
 	new_node->n = n;
 	if (new_node == NULL)
