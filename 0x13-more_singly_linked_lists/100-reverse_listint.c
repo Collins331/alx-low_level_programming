8523e23b0bf786ad87a79760de7d5833c1fa5ec1
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-01 22:02:43 +0300

    reverses the single linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+/**
+  *reverse_listint-  recursive helper function
+  *@head: the previous node
+  *Return: pointer to the first node
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
