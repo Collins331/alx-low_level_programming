56a2a82c8ba2e6b4b9b623a6d1c4899314ff7656
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-03 18:27:38 +0300

    convert binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include"main.h"
+/**
+  *binary_to_uint - converts binary to int
+  *@b: the string to convert
+  *Return: the converted value(success)
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, base = 0;
+
+	while (b[base] != '\0')
+	{
+		if (b[base] != '1' && b[base] != '0')
+		{
+			return (0);
+		}
+		else
+		{
+			if (b[base] == '1')
+			{
+				num = (num << 1) | 1;
+			}
+			else if (b[base] == '0')
+			{
+				num <<= 1;
+			}
+		}
+		base++;
+	}
+	return (num);
+}
