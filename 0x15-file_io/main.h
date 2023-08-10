e8cab2c7439ba213f580ca10b3af019ee69e2a13
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-08-10 12:50:12 +0300

    program that displays the information contained in the ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include<fcntl.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
