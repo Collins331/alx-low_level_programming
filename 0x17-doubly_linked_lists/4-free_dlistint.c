4c28c8a26341433ba1e8142a31206e643dad3877
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-14 21:10:52 +0300

    modified free file

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
-
 /**
  * free_dlistint - frees a dlistint_t list
  * @head: head
- * Return:0 if (success)
+ * Return: nothing
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
+	dlistint_t *tmp;
 
-    if (head != NULL)
-        while (head->prev != NULL)
-            head = head->prev;
-
-    while ((tmp = head) != NULL)
-    {
-        head = head->next;
-        free(tmp);
-    }
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+		head = head->prev;
+	}
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
 }
