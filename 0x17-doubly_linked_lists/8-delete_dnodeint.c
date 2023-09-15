d82445dc5defae479d750b6b3047c5cb05b8397b
Collins Oduor Ochieng <125954874+Collins331@users.noreply.github.com>
2023-09-15 22:41:13 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,8 +26,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = current->next;	
 	if (current->next != NULL)	
 		current->next->prev = current->prev;	
-	if (current->prev != NULL)
-		current->prev->next = current->next;
+	if (current->prev != NULL)	
+		current->prev->next = current->next;	
 	free(current);	
 	return (1);	
 }	
