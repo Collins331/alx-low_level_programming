8600047f41cbac4d37321ec630f2cc06be06117a
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-18 17:17:33 +0300

    modified 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Collins331/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwinner.so
+wget -P /tmp https://github.com/Collins331/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libwinner.so
 export LD_PRELOAD=/tmp/libwinner.so
