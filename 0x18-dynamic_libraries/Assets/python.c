8975c33a2cffeaa026985a819e4c201dcf69a01f
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-09-18 14:03:32 +0300

    added 100-operations.so

---
+++ 0x18-dynamic_libraries/Assets/python.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: variable a
+ * @b: variable b
+ * Return: always an integer(success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two number
+ * @a: variable a
+ * @b: variable b
+ * Return: always an integer(success)
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: variable a
+ * @b: variable b
+ * Return: always interger
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: variable a
+ * @b: variable b
+ * Return: always an integer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - calculates the modulas of two numbers
+ * @a: variable a
+ * @b: variable b
+ * Return: always integer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
