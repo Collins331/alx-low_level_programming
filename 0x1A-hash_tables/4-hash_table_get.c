e9d691ed7b26ff158e503287df15a4f2d81c3c77
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-10-06 21:52:21 +0300

    added 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves the value assoiated with the key
+ * @ht: pointer to the hash table
+ * @key: the key
+ * Return: the value(success) or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
